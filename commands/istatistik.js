7db4d562672c03b0d124479004c5029cb526edde
parsher <dogukanalpt@gmail.com>
2024-06-20 17:38:41 +0300

    istatistikler eklendi

--- commands/istatistik.js
+++ commands/istatistik.js
@@ -6,18 +6,11 @@ exports.run = async (client, message, args) => {
   let embed = new dc.MessageEmbed()
     .setTitle("Parsher Yedek Sistemi")
     .setDescription(`
-  
-  Sunucularınızın yedeklerini oluşturun ve yükleyin
-  
-  **Komutlar**
-  **${prefix}yedek-al**   Yedek Alırsınız.
-  **${prefix}yedek-yükle**   Yedeği Yüklersiniz.
-  **${prefix}yedek-sil**   Yedek Silersiniz.
-  **${prefix}yedek-liste**   Yedek Listenize Bakarsınız.
-  **${prefix}yedek-bilgi**   Belirttiğiniz Yedeğin Özelliklerine Bakarsınız.
-  **${prefix}ping**   Botun pingini gösterir.
-  **${prefix}istatistik**   Botun istatistiklerini gösterir.
-  `)
+      **Bot Istatistikleri**
+
+      ${client.channels.cache.size} Kanal
+      ${client.guilds.cache.size} sunucu
+      toplam ${client.users.cache.size} üye`)
     .setImage("https://media.discordapp.net/attachments/1087824262322212975/1115390850248229014/standard_12.gif?ex=667546ed&is=6673f56d&hm=90d778bcf184bc931c48b260a7e9ea488c4f0655ab550db31585a6f90244ba4d&")
     .setColor("GREEN");
 
